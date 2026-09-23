76c9df26f7b329ad7f0d71bd004d52fff98c1324
Ashish kumar <ashish70235@gmail.com>
2026-09-23 13:55:12 +0530

    Add grade calculation and summary report

--- Task-2-student-Grade/src/Main.java
+++ Task-2-student-Grade/src/Main.java
@@ -7,7 +7,7 @@ public class Main{
 
         ArrayList<Student> students = new ArrayList<>();
 
-        System.out.print("Enter no of students:");
+        System.out.print("Enter no of students: ");
         int n = sc.nextInt();
 
         for( int  i = 0 ; i < n ; i++){
@@ -26,11 +26,49 @@ public class Main{
         }
 
 
-        System.out.println("Student details:");
+//        System.out.println("Student details:");
+//        for( Student student : students){
+//            System.out.println("Name:"+student.name);
+//            System.out.println("Marks:"+student.marks);
+//            System.out.println();
+//        }
+
+
+        double total = 0.0;
+        double highest = students.getFirst().marks;
+        double lowest = students.getFirst().marks;
+
         for( Student student : students){
-            System.out.println("Name:"+student.name);
-            System.out.println("Marks:"+student.marks);
-            System.out.println();
+            total += student.marks;
+
+            if( student.marks > highest){
+                highest = student.marks;
+            }
+
+            if( student.marks < lowest) {
+                lowest = student.marks;
+            }
         }
+
+        double average = total/students.size();
+
+        System.out.print("============= STUDENT SUMMARY REPORT ===============\n\n");
+
+        System.out.printf("%-20s %-10s%n", "Student Name", "Marks");
+        System.out.println("-----------------------------------");
+
+
+        for( Student student : students){
+            System.out.printf("%-20s %-10.2f%n", student.name, student.marks);
+        }
+
+        System.out.println("------------------------------------");
+
+        System.out.printf("Average Marks : %.2f%n", average);
+        System.out.printf("Highest Marks : %.2f%n", highest);
+        System.out.printf("Lowest Marks  : %.2f%n", lowest);
+
+        System.out.println("====================================");
+
     }
 }
